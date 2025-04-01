e580ff7afc9f15be31f9886d4ccb2e2247914262
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-01 14:28:55 +0300

    Update test_routes.py

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -1,1 +1,51 @@
+from http import HTTPStatus
 
+from django.urls import reverse
+
+import pytest
+
+from pytest_django.asserts import assertRedirects
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability(client, name):
+    """Главная страница, страницы регистрации пользователей,
+    входа в учётную запись и выхода из неё доступны анонимным пользователям."""
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_detail_page(client, news):
+    """Страница отдельной новости доступна анонимному пользователю."""
+    url = reverse('news:detail', args=(news.id,))
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_availability_for_comment_edit_and_delete(
+        parametrized_client, expected_status, name, comment
+):
+    """Страницы удаления и редактирования комментария доступны
+    автору комментария.
+    Авторизованный пользователь не может зайти на страницы редактирования
+    или удаления чужих комментариев (возвращается ошибка 404)."""
+    url = reverse(name, args=(comment.id,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
