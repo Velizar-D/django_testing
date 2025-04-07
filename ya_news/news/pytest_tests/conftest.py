5c42388e23c8413baddd1684af953542d2a98e79
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-07 14:04:44 +0300

    Update conftest.py

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 import pytest
 
 from django.test.client import Client
@@ -31,18 +33,39 @@ def not_author_client(not_author):
 
 @pytest.fixture
 def news():
-    news = News.objects.create(
+    new_news = News.objects.create(
         title='Заголовок',
         text='Текст новости',
     )
-    return news
+    return new_news
 
 
 @pytest.fixture
 def comment(author, news):
-    comment = Comment.objects.create(
+    new_comment = Comment.objects.create(
         news=news,
         author=author,
         text='Текст комментария',
     )
-    return comment
+    return new_comment
+
+
+@pytest.fixture
+def list_news():
+    news_list = [
+        News.objects.create(title=f'Заголовок {i}', text=f'Текст новости {i}')
+        for i in range(settings.NEWS_COUNT_ON_HOME_PAGE)
+    ]
+    return news_list
+
+
+@pytest.fixture
+def list_comments(author, news):
+    comments = [
+        Comment.objects.create(
+            news=news,
+            author=author,
+            text=f'Текст комментария {i}'
+        ) for i in range(5)
+    ]
+    return news, comments
