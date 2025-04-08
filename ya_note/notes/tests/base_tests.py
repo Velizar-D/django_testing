c1771f62541b8b48f497fe7357bf4dd633fd694e
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-08 14:08:08 +0300

    Create base_tests.py

---
+++ ya_note/notes/tests/base_tests.py
@@ -0,0 +1,37 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from notes.models import Note
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class BaseTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='author')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+
+        cls.auth_user = User.objects.create(username='auth_user')
+        cls.auth_user_client = Client()
+        cls.auth_user_client.force_login(cls.auth_user)
+
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            author=cls.author,
+            slug='test-slug'
+        )
+
+        cls.URL_HOME = reverse('notes:home')
+        cls.URL_SIGNUP = reverse('users:signup')
+        cls.URL_LOGIN = reverse('users:login')
+        cls.URL_LOGOUT = reverse('users:logout')
+        cls.URL_LIST = reverse('notes:list')
+        cls.URL_SUCCESS = reverse('notes:success')
+        cls.URL_ADD = reverse('notes:add')
+        cls.URL_DETAIL = reverse('notes:detail', args=(cls.note.slug,))
+        cls.URL_EDIT = reverse('notes:edit', args=(cls.note.slug,))
+        cls.URL_DELETE = reverse('notes:delete', args=(cls.note.slug,))
