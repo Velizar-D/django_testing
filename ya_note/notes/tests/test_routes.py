72b47f5b644a7c8cbddcaa0347c27b1f31de9284
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-08 14:26:29 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,105 +1,33 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
-from django.urls import reverse
 
-from notes.models import Note
-
-User = get_user_model()
+from .base_tests import BaseTestCase
 
 
-class AnonymousUserTests(TestCase):
+class TestRoutes(BaseTestCase):
 
-    def test_homepage_availability(self):
-        """Главная страница доступна анонимному пользователю."""
-        url = reverse('notes:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_pages_status_codes(self):
+        """Проверка статус-кодов для разных URL и клиентов."""
+        test_cases = [
+            (self.URL_HOME, self.client, HTTPStatus.OK),
+            (self.URL_SIGNUP, self.client, HTTPStatus.OK),
+            (self.URL_LOGIN, self.client, HTTPStatus.OK),
+            (self.URL_LOGOUT, self.client, HTTPStatus.OK),
 
-    def test_authentication_pages_availability(self):
-        """Страницы регистрации, входа и выхода доступны всем."""
-        urls = (
-            'users:signup',
-            'users:login',
-            'users:logout',
-        )
-        for name in urls:
-            with self.subTest(name=name):
-                url = reverse(name)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
+            (self.URL_LIST, self.author_client, HTTPStatus.OK),
+            (self.URL_SUCCESS, self.author_client, HTTPStatus.OK),
+            (self.URL_ADD, self.author_client, HTTPStatus.OK),
+            (self.URL_DETAIL, self.author_client, HTTPStatus.OK),
+            (self.URL_EDIT, self.author_client, HTTPStatus.OK),
+            (self.URL_DELETE, self.author_client, HTTPStatus.OK),
 
-    def test_redirects_for_anonymous_user(self):
-        """
-        При попытке доступа к защищенным страницам анонимный пользователь
-        перенаправляется на страницу логина.
-        """
-        login_url = reverse('users:login')
-        urls = (
-            ('notes:list', None),
-            ('notes:success', None),
-            ('notes:add', None),
-            ('notes:detail', (1,)),
-            ('notes:edit', (1,)),
-            ('notes:delete', (1,)),
-        )
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                redirect_url = f'{login_url}?next={url}'
-                response = self.client.get(url)
-                self.assertRedirects(response, redirect_url)
+            (self.URL_DETAIL, self.auth_user_client, HTTPStatus.NOT_FOUND),
+            (self.URL_EDIT, self.auth_user_client, HTTPStatus.NOT_FOUND),
+            (self.URL_DELETE, self.auth_user_client, HTTPStatus.NOT_FOUND),
+        ]
 
-
-class AuthorizedUserTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.author = User.objects.create(username='author')
-        cls.author_client = Client()
-        cls.author_client.force_login(cls.author)
-        cls.auth_user = User.objects.create(username='auth_user')
-        cls.auth_user_client = Client()
-        cls.auth_user_client.force_login(cls.auth_user)
-        cls.note = Note.objects.create(
-            title='Заголовок',
-            text='Текст',
-            author=cls.author
-        )
-
-    def test_authorized_user_pages_availability(self):
-        """Аутентифицированному пользователю доступны страницы:
-        списка заметок, успешного добавления, добавления заметки.
-        """
-        urls = (
-            'notes:list',
-            'notes:success',
-            'notes:add',
-        )
-        for name in urls:
-            with self.subTest(name=name):
-                url = reverse(name)
-                response = self.author_client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_note_pages_availability_for_author(self):
-        """Страницы отдельной заметки, редактирования и удаления
-        доступны автору заметки.
-        Если на эти страницы попытается зайти
-        другой пользователь — вернётся ошибка 404.
-        """
-        users_statuses = (
-            (self.author_client, HTTPStatus.OK),
-            (self.auth_user_client, HTTPStatus.NOT_FOUND),
-        )
-        urls = (
-            'notes:detail',
-            'notes:edit',
-            'notes:delete',
-        )
-        for user, status in users_statuses:
-            for name in urls:
-                with self.subTest(user=user, name=name):
-                    url = reverse(name, args=(self.note.slug,))
-                    response = user.get(url)
-                    self.assertEqual(response.status_code, status)
+        for url, client, expected_status in test_cases:
+            with self.subTest(url=url, client=client):
+                response = client.get(url)
+                self.assertEqual(response.status_code, expected_status)
