9cfde8f6a6e2f94a6dfe0a7b5709dac4d91e8f8e
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-08 14:32:22 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -6,7 +6,8 @@ import pytest
 
 @pytest.mark.django_db
 def test_news_count(client, list_news):
-    """Количество новостей на главной странице — не более 10."""
+    """Количество новостей на главной странице — не более 10.
+    """
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
@@ -17,7 +18,8 @@ def test_news_count(client, list_news):
 @pytest.mark.django_db
 def test_news_order(client, list_news):
     """Новости отсортированы от самой свежей к самой старой.
-    Свежие новости в начале списка."""
+    Свежие новости в начале списка.
+    """
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
@@ -29,7 +31,8 @@ def test_news_order(client, list_news):
 @pytest.mark.django_db
 def test_comments_order(client, news, list_comments):
     """Комментарии на странице отдельной новости отсортированы в
-    хронологическом порядке."""
+    хронологическом порядке.
+    """
     url = reverse('news:detail', args=(news.id,))
     response = client.get(url)
     assert 'news' in response.context
@@ -38,15 +41,20 @@ def test_comments_order(client, news, list_comments):
         all_comments = news.comment_set.all()
     else:
         all_comments = []
-    assert all_comments[0].created < all_comments[1].created
+    
+    if len(all_comments) > 1:
+        assert all_comments[0].created < all_comments[1].created
 
 
 @pytest.mark.django_db
 def test_comment_form_access(author_client, not_author_client, client, news):
     """Анонимному пользователю недоступна форма для отправки
-    комментария на странице отдельной новости, а авторизованному доступна."""
+    комментария на странице отдельной новости, а авторизованному доступна.
+    """
     url = reverse('news:detail', args=(news.id,))
+    
     response_anonymous = client.get(url)
     assert 'form' not in response_anonymous.context
+    
     response_author = author_client.get(url)
     assert 'form' in response_author.context
