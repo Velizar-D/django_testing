00a890e47ca73b0a3cd3d328dc5d7edd91a1de40
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-09 11:36:03 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,20 +1,21 @@
+import pytest
+
 from django.conf import settings
 from django.urls import reverse
 
-import pytest
+from news.forms import CommentForm
+from news.models import News
 
 
-@pytest.mark.django_db
 def test_news_count(client, list_news):
     """Количество новостей на главной странице не более 10."""
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
-    news_count = len(object_list)
+    news_count = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-@pytest.mark.django_db
 def test_news_order(client, list_news):
     """Новости отсортированы от самой свежей к самой старой.
     Свежие новости в начале списка.
@@ -22,12 +23,11 @@ def test_news_order(client, list_news):
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
-    all_news = list(object_list)
-    all_news.sort(key=lambda x: x.date, reverse=True)
-    assert all_news == list_news
+    all_news = News.objects.order_by('-date')
+    expected_news = list(all_news[:settings.NEWS_COUNT_ON_HOME_PAGE])
+    assert list(object_list) == expected_news
 
 
-@pytest.mark.django_db
 def test_comments_order(client, news, list_comments):
     """Комментарии на странице отдельной новости отсортированы в
     хронологическом порядке.
@@ -36,21 +36,25 @@ def test_comments_order(client, news, list_comments):
     response = client.get(url)
     assert 'news' in response.context
     news = response.context['news']
-    if news:
-        all_comments = news.comment_set.all()
-    else:
-        all_comments = []
-    if len(all_comments) > 1:
+    all_comments = news.comment_set.all() if news else []
+    if all_comments.count() > 1:
         assert all_comments[0].created < all_comments[1].created
 
 
-@pytest.mark.django_db
-def test_comment_form_access(author_client, not_author_client, client, news):
+def test_comment_form_access_for_anonymous_user(client, news):
     """Анонимному пользователю недоступна форма для отправки
-    комментария на странице отдельной новости, а авторизованному доступна.
+    комментария на странице отдельной новости.
     """
     url = reverse('news:detail', args=(news.id,))
-    response_anonymous = client.get(url)
-    assert 'form' not in response_anonymous.context
-    response_author = author_client.get(url)
-    assert 'form' in response_author.context
+    response = client.get(url)
+    assert 'form' not in response.context
+
+
+def test_comment_form_access_for_authorized_user(author_client, news):
+    """Авторизованному пользователю доступна форма для отправки
+    комментария на странице отдельной новости.
+    """
+    url = reverse('news:detail', args=(news.id,))
+    response = author_client.get(url)
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], CommentForm)
