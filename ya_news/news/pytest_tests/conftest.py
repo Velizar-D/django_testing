1b8a4810916d8009587ff9e745953a9aa850347b
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-09 11:35:49 +0300

    Update conftest.py

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,10 +1,50 @@
-from django.conf import settings
-
 import pytest
 
+from django.conf import settings
 from django.test.client import Client
+from django.urls import reverse
 
 from news.models import Comment, News
+
+
+@pytest.fixture(autouse=True)
+def enable_db_access_for_all_tests(db):
+    pass
+
+
+@pytest.fixture
+def home_url():
+    return reverse('news:home')
+
+
+@pytest.fixture
+def login_url():
+    return reverse('users:login')
+
+
+@pytest.fixture
+def logout_url():
+    return reverse('users:logout')
+
+
+@pytest.fixture
+def signup_url():
+    return reverse('users:signup')
+
+
+@pytest.fixture
+def detail_url(news):
+    return reverse('news:detail', args=(news.id,))
+
+
+@pytest.fixture
+def edit_comment_url(comment):
+    return reverse('news:edit', args=(comment.id,))
+
+
+@pytest.fixture
+def delete_comment_url(comment):
+    return reverse('news:delete', args=(comment.id,))
 
 
 @pytest.fixture
@@ -33,42 +73,35 @@ def not_author_client(not_author):
 
 @pytest.fixture
 def news():
-    new_news = News.objects.create(
-        title='Заголовок',
-        text='Текст новости',
-    )
-    return new_news
+    return News.objects.create(title='Заголовок', text='Текст новости',)
 
 
 @pytest.fixture
 def comment(author, news):
-    new_comment = Comment.objects.create(
+    return Comment.objects.create(
         news=news,
         author=author,
         text='Текст комментария',
-    )
-    return new_comment
+        )
 
 
 @pytest.fixture
 def list_news():
-    news_list = [
-        News.objects.create(title=f'Заголовок {i}', text=f'Текст новости {i}')
-        for i in range(settings.NEWS_COUNT_ON_HOME_PAGE)
-    ]
-    return news_list
+    for i in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+        News.objects.create(
+            title=f'Заголовок {i}',
+            text=f'Текст новости {i}'
+        )
 
 
 @pytest.fixture
 def list_comments(author, news):
-    comments = [
+    for i in range(5):
         Comment.objects.create(
             news=news,
             author=author,
             text=f'Текст комментария {i}'
-        ) for i in range(5)
-    ]
-    return news, comments
+        )
 
 
 @pytest.fixture
