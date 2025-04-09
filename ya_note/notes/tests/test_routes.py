04973bdf6f7246b342d09aead507f5b7a67da712
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-09 15:28:53 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -29,3 +29,22 @@ class TestRoutes(BaseTestCase):
             with self.subTest(url=url, client=client):
                 response = client.get(url)
                 self.assertEqual(response.status_code, expected_status)
+
+    def test_redirects_for_anonymous_user(self):
+        """
+        Анонимный пользователь перенаправляется на страницу логина
+        при попытке доступа к защищенным страницам.
+        """
+        urls = (
+            self.URL_LIST,
+            self.URL_SUCCESS,
+            self.URL_ADD,
+            self.URL_DETAIL,
+            self.URL_EDIT,
+            self.URL_DELETE,
+        )
+        for url in urls:
+            with self.subTest(url=url):
+                redirect_url = f'{self.URL_LOGIN}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
