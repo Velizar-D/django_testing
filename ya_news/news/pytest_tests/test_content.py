130f3c7950fb8a89f29864b1c9fc6a39ee0bc1ca
Velizar-D <38582349+Velizar-D@users.noreply.github.com>
2025-04-10 12:32:59 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,58 +1,55 @@
 from django.conf import settings
-from django.urls import reverse
 
 from news.forms import CommentForm
 from news.models import News
 
 
-def test_news_count(client, list_news):
+def test_news_count(client, list_news, home_url):
     """Количество новостей на главной странице не более 10."""
-    url = reverse('news:home')
-    response = client.get(url)
+    response = client.get(home_url)
     object_list = response.context['object_list']
     news_count = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-def test_news_order(client, list_news):
+def test_news_order(client, list_news, home_url):
     """Новости отсортированы от самой свежей к самой старой.
     Свежие новости в начале списка.
     """
-    url = reverse('news:home')
-    response = client.get(url)
+    response = client.get(home_url)
     object_list = response.context['object_list']
-    all_news = News.objects.order_by('-date')
-    expected_news = list(all_news[:settings.NEWS_COUNT_ON_HOME_PAGE])
-    assert list(object_list) == expected_news
+    all_news = News.objects.order_by('-date')[
+        :settings.NEWS_COUNT_ON_HOME_PAGE
+        ]
+    assert list(object_list) == list(all_news)
 
 
-def test_comments_order(client, news, list_comments):
+def test_comments_order(client, news, list_comments, detail_url):
     """Комментарии на странице отдельной новости отсортированы в
     хронологическом порядке.
     """
-    url = reverse('news:detail', args=(news.id,))
-    response = client.get(url)
+    response = client.get(detail_url)
     assert 'news' in response.context
     news = response.context['news']
-    all_comments = news.comment_set.all() if news else []
+    all_comments = news.comment_set.all()
     if all_comments.count() > 1:
-        assert all_comments[0].created < all_comments[1].created
+        for i in range(1, all_comments.count()):
+            assert all_comments[i - 1].created < all_comments[i].created
 
 
-def test_comment_form_access_for_anonymous_user(client, news):
+def test_comment_form_access_for_anonymous_user(client, news, detail_url):
     """Анонимному пользователю недоступна форма для отправки
     комментария на странице отдельной новости.
     """
-    url = reverse('news:detail', args=(news.id,))
-    response = client.get(url)
+    response = client.get(detail_url)
     assert 'form' not in response.context
 
 
-def test_comment_form_access_for_authorized_user(author_client, news):
+def test_comment_form_access_for_authorized_user(author_client, news,
+                                                 detail_url):
     """Авторизованному пользователю доступна форма для отправки
     комментария на странице отдельной новости.
     """
-    url = reverse('news:detail', args=(news.id,))
-    response = author_client.get(url)
+    response = author_client.get(detail_url)
     assert 'form' in response.context
     assert isinstance(response.context['form'], CommentForm)
